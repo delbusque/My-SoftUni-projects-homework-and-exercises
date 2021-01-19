0c15742f620406268f4be3f7b813571cd08d4826
delbusque <del.busque@gmail.com>
2021-01-19 17:55:46 +0200

    Added TruckTour

---
+++ C#Advanced/01-StacksAndQueues-Excersise/07.TruckTourr/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace _08.BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPumps = int.Parse(Console.ReadLine());
+
+            Queue<int[]> pumps = new Queue<int[]>();
+
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                string pumpInput = $"{Console.ReadLine()} {i}";
+                int[] pump = pumpInput.Split().Select(int.Parse).ToArray();
+
+                pumps.Enqueue(pump);
+            }
+
+            var tank = 0;
+
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                int[] currPump = pumps.Peek();
+                tank += currPump[0];
+
+                if (tank >= currPump[1])
+                {
+                    tank -= currPump[1];
+                    pumps.Enqueue(pumps.Dequeue());
+                }
+                else
+                {
+                    pumps.Enqueue(pumps.Dequeue());
+                    tank = 0;
+                    i = -1;
+                }
+            }
+
+            int[] thePump = pumps.Peek();
+            Console.WriteLine(string.Join("", thePump[2]));
+        }
+    }
+}
