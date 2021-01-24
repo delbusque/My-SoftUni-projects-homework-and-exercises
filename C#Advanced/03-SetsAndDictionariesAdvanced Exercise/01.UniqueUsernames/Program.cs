81393e55b41999fc2b6fb04beff365159d9f8167
delbusque <del.busque@gmail.com>
2021-01-24 15:02:18 +0200

    Added Exercise work

---
+++ C#Advanced/03-SetsAndDictionariesAdvanced Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> userNames = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                
+                if (!userNames.Contains(name))
+                {
+                    userNames.Add(name);
+                }
+            }
+
+            foreach (var item in userNames)
+            {              
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ C#Advanced/03-SetsAndDictionariesAdvanced Exercise/02. SetsOfElements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] param = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            HashSet<int> setA = new HashSet<int>();
+            HashSet<int> setB = new HashSet<int>();
+
+            for (int i = 0; i < param[0]; i++)
+            {
+                setA.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < param[1]; i++)
+            {
+                setB.Add(int.Parse(Console.ReadLine()));
+            }
+
+            setA.IntersectWith(setB);
+
+            Console.Write(string.Join(" ", setA)); 
+        }
+    }
+}
