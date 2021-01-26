ce06a0c8af37ae2579fa6f3950f8ab8fe1baa65b
delbusque <del.busque@gmail.com>
2021-01-26 15:22:12 +0200

    Knight Game is not resolved

---
+++ C#Advanced/02-MultidimensionalArraysExercise/08.Bombs/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08.Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short size = short.Parse(Console.ReadLine());
+            short[,] field = new short[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                short[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(short.Parse).ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    field[row, col] = input[col];
+                }
+            }
+
+            string[] bombsInput = Console.ReadLine().Split();
+
+            for (int i = 0; i < bombsInput.Length; i++)
+            {
+                short[] bomb = bombsInput[i].Split(",", StringSplitOptions.RemoveEmptyEntries).Select(short.Parse).ToArray();
+                short row = bomb[0];
+                short col = bomb[1];
+
+                if (field[row,col] > 0)
+                {
+                    if (col - 1 >= 0 && field[row, col - 1] > 0)
+                    {
+                        field[row, col - 1] -= field[row, col];
+                    }
+                    if (col+1 <= field.GetLength(1)-1 && field[row, col + 1] > 0)
+                    {
+                        field[row, col + 1] -= field[row, col];
+                    }
+                    if (row-1 >= 0 && col-1 >= 0 && field[row - 1, col - 1] > 0)
+                    {
+                        field[row - 1, col - 1] -= field[row, col];
+                    }
+                    if (row-1 >= 0 && field[row - 1, col] > 0)
+                    {
+                        field[row - 1, col] -= field[row, col];
+                    }
+                    if (row-1 >= 0 && col + 1 <= field.GetLength(1)-1 && field[row - 1, col + 1] > 0)
+                    {
+                        field[row - 1, col + 1] -= field[row, col];
+                    }
+                    if (row+1 <= field.GetLength(0)-1 && col-1 >= 0 && field[row + 1, col - 1] > 0)
+                    {
+                        field[row + 1, col - 1] -= field[row, col];
+                    }
+                    if (row+1 <= field.GetLength(0)-1 && field[row + 1, col] > 0)
+                    {
+                        field[row + 1, col] -= field[row, col];
+                    }
+                    if (row+1 <= field.GetLength(0) && col+1 <= field.GetLength(1)-1 && field[row + 1, col + 1] > 0)
+                    {
+                        field[row + 1, col + 1] -= field[row, col];
+                    }  
+                    
+                    field[row, col] -= field[row, col];
+                }
+                
+            }
+
+            int sum = 0;
+            int count = 0;
+
+            foreach (var item in field)
+            {
+                if (item > 0)
+                {
+                    sum += item;
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {count}");
+            Console.WriteLine($"Sum: {sum}");
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write($"{field[i,j]} ");
+                }
+
+                Console.WriteLine();
+            }          
+        }
+    }
+}
---
+++ C#Advanced/02-MultidimensionalArraysExercise/ConsoleApp1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] board = new char[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < size; col++)
+                {
+                    board[row, col] = input[col];
+                }
+            }
+
+            Dictionary<int, int> horses = new Dictionary<int, int>();
+
+            int horseNumber = 0;
+            int attacks = 0;
+            int removalCount = 0;
+
+            for (int r = 0; r < size; r++)
+            {
+                for (int c = 0; c < size; c++)
+                {
+                    if (board[r, c] == 'K')
+                    {
+                        horseNumber++;
+                        horses.Add(horseNumber, 0);
+
+                        if (r - 1 >= 0 && c + 2 < board.GetLength(1))
+                        {
+                            if (board[r - 1, c + 2] == 'K')
+                            {
+                                horses[horseNumber]++;
+                            }
+                        }                       
+                        if (r - 2 >= 0 && c + 1 < board.GetLength(1))
+                        {
+                            if (board[r - 2, c + 1] == 'K')
+                            {
+                                horses[horseNumber]++;
+                            }
+                        }                        
+
+                        if (r + 1 < board.GetLength(0) && c - 2 >= 0)
+                        {
+                            if (board[r + 1, c - 2] == 'K')
+                            {
+                                horses[horseNumber]++;
+                            }
+                        }                       
+                        if (r + 2 < board.GetLength(0) && c - 1 >= 0)
+                        {
+                            if (board[r + 2, c - 1] == 'K')
+                            {
+                                horses[horseNumber]++;
+                            }
+                        }
+
+                        if (r + 2 < size && c + 1 < size)
+                        {
+                            if (board[r + 2, c + 1] == 'K')
+                            {
+                                horses[horseNumber]++;
+                            }
+                        }
+                        if (r + 1 < size && c + 2 < size)
+                        {
+                            if (board[r + 1, c + 2] == 'K')
+                            {
+                                horses[horseNumber]++;
+                            }
+                        }
+                        
+                        if (r - 2 >= 0 && c - 1 >= 0)
+                        {
+                            if (board[r - 2, c - 1] == 'K')
+                            {
+                                horses[horseNumber]++;
+                            }
+                        }
+                        if (r - 1 >= 0 && c - 2 >= 0)
+                        {
+                            if (board[r - 1, c - 2] == 'K')
+                            {
+                                horses[horseNumber]++;
+                            }
+                        }
+
+                        
+                        attacks += horses[horseNumber];
+                    }
+                }
+            }
+
+            attacks /= 2;
+
+            while (attacks > 0)
+            {
+                foreach (var item in horses.OrderByDescending(h => h.Value))
+                {
+                    attacks -= item.Value;
+                    
+                    removalCount++;
+
+
+                    if (attacks == 0)
+                    {
+                        break;
+                    }
+                    else if (attacks < 0)
+                    {
+                        removalCount = removalCount + Math.Abs(attacks) + 1;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(removalCount);
+        }
+    }
+}
