317be72b97c2a71186ffd02e9d53b034216f51a4
delbusque <del.busque@gmail.com>
2021-02-02 16:14:26 +0200

    Finished Exercise

---
+++ C#Advanced/04-StreamsFilesAndDirectoriesExercise/05.DirectoryTraversal/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _05.DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> files = new Dictionary<string,Dictionary<string,double>>();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo("../../../");
+
+            var list = directoryInfo.GetFiles();
+
+            foreach (var file in list)
+            {
+                if (!files.ContainsKey(file.Extension))
+                {
+                    files.Add(file.Extension, new Dictionary<string, double>());
+                    files[file.Extension].Add(file.Name, file.Length);
+                }
+                else
+                {
+                    files[file.Extension].Add(file.Name, file.Length / 1024.00);
+                }
+               
+            }
+
+            var path = Path.Combine($"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}", "output.txt");
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                foreach (var type in files.OrderByDescending(t => t.Value.Count).ThenBy(x => x.Key))
+                {
+                    writer.WriteLine(type.Key);
+
+                    foreach (var item in type.Value.OrderByDescending(x => x.Value))
+                    {
+                        writer.WriteLine($"--{item.Key} - {item.Value:f3}kb");
+                    }
+                }
+            }          
+        }
+    }
+}
---
+++ C#Advanced/04-StreamsFilesAndDirectoriesExercise/06.ZipAndExtract/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZipFile.CreateFromDirectory("data", "output.zip");
+
+            ZipFile.ExtractToDirectory("output.zip", Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+        }
+    }
+}
