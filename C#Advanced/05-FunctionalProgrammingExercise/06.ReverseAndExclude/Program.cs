524ed23ce7a20644a3c751d086cbcc2651467536
delbusque <del.busque@gmail.com>
2021-02-04 17:22:33 +0200

    Added new problems

---
+++ C#Advanced/05-FunctionalProgrammingExercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(int.Parse).Reverse().ToList();
+
+            int numb = int.Parse(Console.ReadLine());
+            
+            Predicate<int> divisible = n => n % numb == 0;
+            input.RemoveAll(divisible);
+
+            Console.WriteLine(string.Join(" ", input));
+        }   
+    }
+}
---
+++ C#Advanced/05-FunctionalProgrammingExercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Func<string, int, bool> func = (name, lenght) => name.Length <= lenght;
+
+            names = names.Where(name => func(name, n)).ToList();
+                       
+            Action<List<string>> print = name => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            print(names);      
+        }        
+    }
+}
