029c65a132570a22d4c221ff77a94e6e6b0a4d99
delbusque <del.busque@gmail.com>
2021-02-11 16:18:43 +0200

    Added Generics Lab

---
+++ C#Advanced/08-Generics/BoxOfT/BoxOfT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class BoxOfT<T>
+    {
+        Stack<T> stack;
+        
+        public BoxOfT()
+        {
+            stack = new Stack<T>();
+        }
+
+        public int Count { get
+            {
+                return stack.Count;
+            }
+        }
+
+        public void Add( T element)
+        {
+            stack.Push(element);
+        }
+
+        public T Remove()
+        {
+            return stack.Pop();            
+        }
+    }
+}
---
+++ C#Advanced/08-Generics/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T element)
+        {
+            T[] arr = new T[lenght];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = element;
+            }
+
+            return arr;
+        }
+
+    }
+}
