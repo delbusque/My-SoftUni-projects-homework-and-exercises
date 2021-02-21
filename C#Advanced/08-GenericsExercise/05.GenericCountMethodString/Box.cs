cda96a596f5a0ceeaefcb3c3ec9404e9cc2b6326
delbusque <del.busque@gmail.com>
2021-02-21 14:18:10 +0200

    Added new method

---
+++ C#Advanced/08-GenericsExercise/05.GenericCountMethodString/Box.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.GenericCountMethodString
+{
+    public class Box<T>
+        where T : IComparable
+    {       
+        public List<T> Values { get; set; }
+
+        public Box()
+        {
+            Values = new List<T>();
+        }
+
+        public int GreaterValues(T element)
+        {
+            int count = 0;
+            
+            foreach (var item in Values)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in Values)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            string result = sb.ToString().TrimEnd();
+            return result;
+        }
+
+
+
+
+    }
+}
--- C#Advanced/08-GenericsExercise/05.GenericCountMethodString/Program.cs
+++ C#Advanced/08-GenericsExercise/05.GenericCountMethodString/Program.cs
@@ -2,11 +2,23 @@
 
 namespace _05.GenericCountMethodString
 {
-    class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            Box<string> line = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                line.Values.Add(input);
+            }
+
+            string element = Console.ReadLine();
+
+            Console.WriteLine(line.GreaterValues(element));
         }
     }
 }
