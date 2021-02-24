1af6050abb53bb5d3c29022b12f4047c88384ba6
delbusque <del.busque@gmail.com>
2021-02-24 19:05:54 +0200

    Added an Iterators and Comparators problem

---
+++ C#Advanced/09-IteratorsAndComparators/IteratorsAndComparators/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public Book(string name, int year, params string[] authors)
+        {
+            Name = name;
+            Year = year;
+            Authors = authors;
+
+        }
+        
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public IReadOnlyList<string> Authors { get; set; }
+
+
+    }
+}
---
+++ C#Advanced/09-IteratorsAndComparators/IteratorsAndComparators/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var item in libraryTwo)
+            {
+                
+                if (item.Authors.Count == 0)
+                {
+                    Console.WriteLine($"{item.Name}, {item.Year}");
+                }
+                else
+                {
+                    Console.WriteLine($"{item.Name}, {item.Year}, {string.Join(" / ", item.Authors)}");
+                }
+            }         
+        }
+    }
+}
