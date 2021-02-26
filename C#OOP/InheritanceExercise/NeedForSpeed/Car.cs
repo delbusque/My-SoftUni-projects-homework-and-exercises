add465f62a21d43b5ab50895f3f24ba0f02cdb5e
delbusque <del.busque@gmail.com>
2021-02-26 12:09:33 +0200

    More inheritance training

---
+++ C#OOP/InheritanceExercise/NeedForSpeed/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        public Car(int horsePower, double fuel)
+            :base(horsePower, fuel)
+        {
+            DefaultFuelConsumption = 3;
+        }
+
+        public override double FuelConsumption => DefaultFuelConsumption;
+    }
+}
