5bea28ceabbea0a42d6fa24534fe0f4497b6f7ce
Ventsislav Peychev <del.busque@gmail.com>
2021-03-09 10:51:14 +0200

    Added Cars

---
+++ C#OOP/InterfacesAndAbstraction/Cars/Seat.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+        
+        public string Model { get ; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] getType = this.GetType().ToString().Split('.');
+            sb.AppendLine($"{Color} {getType[1]} {Model}");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#OOP/InterfacesAndAbstraction/Cars/Tesla.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Model { get; set ; }
+        public string Color { get ; set ; }
+        public int Battery { get ; set ; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] getType = this.GetType().ToString().Split('.');
+            sb.AppendLine($"{Color} {getType[1]} {Model} with {Battery} Batteries");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+
+            return sb.ToString();
+        }
+    }
+}
