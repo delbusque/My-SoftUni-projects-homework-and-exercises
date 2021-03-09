9521a526dd376e5af123064fb9960a7d5f961a83
delbusque <del.busque@gmail.com>
2021-03-09 17:34:16 +0200

    Added Person Info

---
+++ C#OOP/InterfacesAndAbstractionExercise/PersonInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+
+
+            IBirthable person = new Citizen(name, age, id, birthdate);
+            IIdentifiable citizen = new Citizen(name, age, id, birthdate);
+            
+            Console.WriteLine(citizen.Id);           
+            Console.WriteLine(person.Birthdate);
+        }
+    }
+}
