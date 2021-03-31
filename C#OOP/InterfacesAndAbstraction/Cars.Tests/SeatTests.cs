f6d03d262c2ad1bd3ff1b7ae9738026d55da76c7
Ventsislav Peychev <del.busque@gmail.com>
2021-03-31 13:21:56 +0300

    Added Unit Testing

---
+++ C#OOP/InterfacesAndAbstraction/Cars.Tests/SeatTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Cars.Tests
+{
+    [TestFixture]
+    public class SeatTests
+    {
+        private Seat seat;
+
+
+        [SetUp]
+        public void Initializer()
+        {
+            seat = new Seat("Hello", "red");
+        }
+
+        [Test]
+        public void IsEngineStarting()
+        {
+            Assert.That(() => seat.Start(), Is.EqualTo("Engine start"));
+        }
+
+        [Test]
+        public void IsModelCorrectlyInitialized()
+        {
+            Assert.That(seat.Model, Is.EqualTo("Hello"));
+        }        
+    }
+}
