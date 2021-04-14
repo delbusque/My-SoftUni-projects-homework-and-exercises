c35ceb4f74f52847d8208e626a4c6271e1f24732
Ventsislav Peychev <del.busque@gmail.com>
2021-04-14 19:20:57 +0300

    Added Advanced Retake

---
+++ C#Advanced/AdvancedRetake-April2021/AdvancedRetakeApril2021/01.WarmWinter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.WarmWinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> hats = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            Queue<int> scarfs = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            List<int> sets = new List<int>();
+
+            while (hats.Count > 0 && scarfs.Count > 0)
+            {
+                int hat = hats.Peek();
+                int scarf = scarfs.Peek();
+
+                if (hat > scarf)
+                {
+                    sets.Add(hats.Pop() + scarfs.Dequeue());                    
+                }
+
+                else if (scarf > hat)
+                {
+                    hats.Pop();
+                }
+
+                else
+                {
+                    scarfs.Dequeue();
+                    int currentHat = hats.Pop() + 1;
+                    hats.Push(currentHat);
+                }
+            }
+
+            List<int> ordered = sets.OrderByDescending(s => s).ToList();
+
+            foreach (var item in ordered)
+            {
+                Console.WriteLine($"The most expensive set is: {item}");
+                break;
+            }
+
+            foreach (var item in sets)
+            {              
+                Console.Write($"{item} ");
+            }          
+        }
+    }
+}
---
+++ C#Advanced/AdvancedRetake-April2021/AdvancedRetakeApril2021/03.CoctailParty/Skeleton/Cocktail.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        private List<Ingredient> ingredients;
+
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+            ingredients = new List<Ingredient>();
+        }
+
+        //public List<Ingredient> Ingredients { get; set; }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int MaxAlcoholLevel { get; set; }
+
+        public int CurrentAlcoholLevel { 
+            
+            get
+            {
+                int alcoholLevel = 0;
+
+                foreach (var item in ingredients)
+                {
+                    alcoholLevel += item.Alcohol;
+                }
+
+                return alcoholLevel;
+            }
+        }
+
+        public void Add(Ingredient ingredient)
+        {
+            if (!ingredients.Contains(ingredient))
+            {
+                
+                if (CurrentAlcoholLevel <= MaxAlcoholLevel && ingredient.Quantity <= Capacity)
+                {
+                    ingredients.Add(ingredient);
+                }
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Ingredient toRemove = ingredients.FirstOrDefault(i => i.Name == name);
+
+            return ingredients.Remove(toRemove);
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            Ingredient toReturn = ingredients.FirstOrDefault(i => i.Name == name);
+
+            return toReturn;
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            foreach (var item in ingredients.OrderByDescending(i => i.Alcohol))
+            {
+                return item;
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+
+            foreach (var item in ingredients)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
