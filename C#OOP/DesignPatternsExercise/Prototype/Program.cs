26ac132d7d5da55f870b2b81ad3f5efc172bb484
Ventsislav Peychev <del.busque@gmail.com>
2021-05-06 13:03:55 +0300

    Added Prototype Design Pattern

---
+++ C#OOP/DesignPatternsExercise/Prototype/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sandwich sandwich = new Sandwich("grain", "rockfor", "chicken", "onion, spinach");
+            Sandwich prototypeSandwich = (Sandwich)sandwich.Clone();
+
+            Console.WriteLine(prototypeSandwich.GetIngredients());
+
+            SandwichMenu sandwiches = new SandwichMenu();
+
+            sandwiches["Chicken"] = new Sandwich("grain", "rockfor", "chicken", "onion");
+            sandwiches["Beef"] = new Sandwich("grain", "mature", "beef", "onion");
+            sandwiches["Veggie"] = new Sandwich("grain", "", "", "onion, spinach");
+
+            sandwiches["LoadedChicken"] = new Sandwich("rye", "gauda, mature", "chicken", "onion");
+            sandwiches["ThreeMeat"] = new Sandwich("wheat", "swiss", "beef, bacon, ham", "tomato");
+
+            Sandwich sandwich1 = sandwiches["Beef"].Clone() as Sandwich;
+            Sandwich sandwich2 = sandwiches["Veggie"].Clone() as Sandwich;
+            Sandwich sandwich3 = sandwiches["ThreeMeat"].Clone() as Sandwich;
+
+            Console.WriteLine(sandwich3.GetIngredients());
+
+        }
+    }
+}
---
+++ C#OOP/DesignPatternsExercise/Prototype/Sandwich.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Sandwich : SandwichPrototype
+    {
+        private string bread;
+        private string cheese;
+        private string meat;
+        private string veggies;
+
+        public Sandwich(string bread, string cheese, string meat, string veggies)
+        {
+            this.bread = bread;
+            this.cheese = cheese;
+            this.meat = meat;
+            this.veggies = veggies;
+        }
+
+        public string GetIngredients()
+        {
+            return $"Sandwich with {bread} bread, {meat}, {cheese}, {veggies}";
+        }
+
+        public override SandwichPrototype Clone()
+        {
+            return MemberwiseClone() as SandwichPrototype;
+        }
+    }
+}
