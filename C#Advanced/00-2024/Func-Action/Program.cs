88df06c4fbccbc287788c02b357450aec1dc6f33
Ventsislav Peychev <del.busque@gmail.com>
2024-08-15 10:03:00 +0300

    Added Func<T,R>, Action<T>

---
+++ C#Advanced/00-2024/Func-Action/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Func<int, int> multiply = Multi;    
+        Action<string> text = word => Console.WriteLine(word);
+
+        Console.WriteLine(multiply(10));
+        text("Wtf");
+
+        string[] names = { "Eathan", "Lucas", "Noah", "StanleyRoyce" };
+
+        Array.ForEach(names, printNames);
+    }
+
+static Action<string> printNames = name => Console.WriteLine($"Sir {name}");
+
+public static int Multi(int b)
+    {
+        return b * 100;
+    }
+
+}
