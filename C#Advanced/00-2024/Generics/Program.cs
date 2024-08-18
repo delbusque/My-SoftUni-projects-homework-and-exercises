ad05eee888e58b4a1c9d87434eab4c54e08f48b6
Ventsislav Peychev <del.busque@gmail.com>
2024-08-18 12:19:24 +0300

    Add Generics

---
+++ C#Advanced/00-2024/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿Equals<int> equality = new (1, 1);
+
+Console.WriteLine(equality.AreEqual());
+
+Dictionary<string, int> dic = new (){{"1",1}, { "12", 1 } };
+
+
+public class Equals<T>
+    where T : struct
+{
+    private T first;
+    private T second;
+
+    public Equals(T first, T second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public bool AreEqual()
+    {
+        return first.Equals(second);
+    }
+}
