65b4dfadf8b68bc383c7dccf643ad719e4042062
Ventsislav Peychev <del.busque@gmail.com>
2024-08-21 15:25:45 +0300

    Added static ForEach

---
+++ C#Advanced/00-2024/Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Eathan", "Lucas", "Noah", "StanleyRoyce" };
+
+            Array.ForEach(names, printNames);
+        }
+
+        static Action<string> printNames = name => Console.WriteLine($"Sir {name}");
+
+
+    }
+}
