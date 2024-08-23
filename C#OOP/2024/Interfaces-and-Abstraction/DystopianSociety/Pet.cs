0f829ea4a08ebcbb426923d4c176d19bbb836e55
delbusque <del.busque@gmail.com>
2024-08-23 10:10:42 +0300

    Create Pet.cs

---
+++ C#OOP/2024/Interfaces-and-Abstraction/DystopianSociety/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DystopianSociety
+{
+    public class Pet : ILivingCreater
+    {
+        public Pet(string name, DateOnly birthday)
+        {
+            Name = name;
+            Birthday = birthday;
+        }
+
+        public string Name { get; private set; }
+
+        public DateOnly Birthday { get; private set; }
+    }
+}
