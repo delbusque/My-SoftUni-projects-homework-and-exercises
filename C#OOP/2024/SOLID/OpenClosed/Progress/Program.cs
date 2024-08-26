157d1bcab951974a547eca52870be96f736ccc7f
Ventsislav Peychev <del.busque@gmail.com>
2024-08-26 11:27:42 +0300

    Update Program.cs

--- C#OOP/2024/SOLID/OpenClosed/Progress/Program.cs
+++ C#OOP/2024/SOLID/OpenClosed/Progress/Program.cs
@@ -4,9 +4,5 @@ IProgressable file = new CustomFile();
 file.Length = 1000;
 file.Sent = 1;
 
-IProgressable song = new Music();
-song.Length = 4300;
-song.Sent = 12;
-
-Progress progress = new Progress(song);
+Progress progress = new Progress(new Music() { Length = 2000, Sent = 330});
 progress.currentProgress();
