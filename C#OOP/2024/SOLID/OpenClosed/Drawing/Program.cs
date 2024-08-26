2cf1ea9d09c1981f492b44562625b7c43f49bae6
Ventsislav Peychev <del.busque@gmail.com>
2024-08-26 12:47:29 +0300

    Update Program.cs

--- C#OOP/2024/SOLID/OpenClosed/Drawing/Program.cs
+++ C#OOP/2024/SOLID/OpenClosed/Drawing/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace Drawing
+﻿using Drawing.Contracts;
+
+namespace Drawing
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            IShape circle = new Circle();
+
             DrawingMachine machine = new DrawingMachine();
+            
+            machine.Draw(circle);
             machine.Draw(new Trapec());
+            
         }
     }
 }
